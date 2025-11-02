725f6914a82068fdaf8a1624edeb42e69e77cc2f
antonpohodun <antony.hopkins008@gmail.com>
2025-11-02 14:27:59 +0200

    Parse data

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello")
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from dotenv import load_dotenv
+
+load_dotenv()
+
+documents = SimpleDirectoryReader("data").load_data()
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+response = query_engine.query("How many cvs are there?")
+
+print(response)
